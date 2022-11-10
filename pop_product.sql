f84389003559a34744236d1fee5841d30a59d90c
swt <98679146+DomeSirawit@users.noreply.github.com>
2022-11-10 17:20:50 +0700

    Create pop_product.sql

---
+++ pop_product.sql
@@ -0,0 +1,17 @@
+--popular_products
+{%set groups = ["batteries", "laptop", "headphone"] %}
+
+WITH products_with_group AS
+(SELECT
+    *, 
+    CASE 
+    {% for group in groups %}
+    WHEN lower(Product) LIKE '%{{ group }}%' THEN '{{group}}'
+    {% endfor %}
+    ELSE 'other'
+    END AS product_group
+FROM {{ ref('sales_tb') }})
+SELECT product_group, SUM(Quantity_Ordered) AS sale_count
+FROM products_with_group 
+GROUP BY 1
+ORDER BY sale_count DESC
